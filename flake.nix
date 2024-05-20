00294364b83a2faad6cf08bd13a6f3db5f4aca27
Christina Sørensen <christina@cafkafk.com>
2024-05-20 17:57:43 +0200

    build: introduce flake.nix
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

---
+++ flake.nix
@@ -0,0 +1,48 @@
+# This file is stolen from @IvanMalison and @LSLeary (github)
+{
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    git-ignore-nix.url = github:hercules-ci/gitignore.nix/master;
+    xmonad.url = github:xmonad/xmonad;
+    xmonad-contrib.url = github:xmonad/xmonad-contrib;
+  };
+  outputs = { self, flake-utils, nixpkgs, git-ignore-nix, xmonad, xmonad-contrib }:
+  with xmonad.lib;
+  let
+    hoverlay = final: prev: hself: hsuper: {
+      xmonad-extra = hself.callCabal2nix "xmonad-extra"
+        (git-ignore-nix.lib.gitignoreSource ./.) { };
+    };
+    defComp = if builtins.pathExists ./comp.nix
+      then import ./comp.nix
+      else { };
+    overlay = fromHOL hoverlay defComp;
+    overlays = [ overlay (fromHOL xmonad.hoverlay defComp) xmonad-contrib.overlay];
+    nixosModule = { config, lib, ... }: with lib;
+      let
+        cfg = config.services.xserver.windowManager.xmonad;
+        comp = { inherit (cfg.flake) prefix compiler; };
+      in {
+        config = mkIf (cfg.flake.enable && cfg.enableContribAndExtras) {
+          nixpkgs.overlays = [ (fromHOL hoverlay comp) ];
+        };
+      };
+    nixosModules = [ nixosModule ] ++ xmonad.nixosModules;
+  in flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system overlays; };
+    hpkg = pkgs.lib.attrsets.getAttrFromPath (hpath defComp) pkgs;
+    modifyDevShell =
+      if builtins.pathExists ./develop.nix
+      then import ./develop.nix
+      else _: x: x;
+  in
+  rec {
+    devShell = hpkg.shellFor (modifyDevShell pkgs {
+      packages = p: [ p.xmonad-extra ];
+      nativeBuildInputs = [ pkgs.cabal-install ];
+    });
+    defaultPackage = hpkg.xmonad-extra;
+    modernise = xmonad.modernise.${system};
+  }) // { inherit hoverlay overlay overlays nixosModule nixosModules; } ;
+}
